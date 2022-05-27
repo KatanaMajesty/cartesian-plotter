364d3ed99a5e9f2d1116158d171576387e0534ce
KatanaMajesty <sasha.panasenk@gmail.com>
2022-05-27 19:15:00 +0300

    Added Primitives, Implemented Dynamic Batch rendering & Code Refactoring

--- Source/Core/Renderer.cpp
+++ Source/Core/Renderer.cpp
@@ -1,4 +1,5 @@
 #include <Core/Renderer.h>
+#include <Core/Window.h>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -27,11 +28,16 @@ void ImGuiInit(GLFWwindow* context)
     ImGui_ImplOpenGL3_Init("#version 130");
 }
 
+std::shared_ptr<Shader> basicShader;
+glm::mat4 projection;
+glm::mat4 view;
+glm::mat4 model;
+
 // Overall data
-Renderer::Renderer(GLFWwindow* context, size_t vertices)
-: m_Context(context), m_Vertices(vertices)
+Renderer::Renderer(Window* const window, size_t vertices)
+: m_Window(window), m_Vertices(vertices)
 {
-	ImGuiInit(m_Context);
+	ImGuiInit(m_Window->Context());
 
 	if (glewInit() != GLEW_OK)
 	{
@@ -48,23 +54,51 @@ Renderer::Renderer(GLFWwindow* context, size_t vertices)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, position)));
 
-	MapShader("Basic");
+	basicShader = MapShader("Basic");
+
+	// Setup axes
+	Line xAxis;
+	xAxis.a = { glm::vec2(-1.0f, 0.0f) };
+	xAxis.b = { glm::vec2(1.0f, 0.0f) };
+
+	Line yAxis;
+	yAxis.a = { glm::vec2(0.0f, -1.0f) };
+	yAxis.b = { glm::vec2(0.0f, 1.0f) };
+
+	Quad quad;
+	quad.a = { glm::vec2(-0.5f, 0.5f) };
+	quad.b = { glm::vec2(0.5f, 0.5f) };
+	quad.c = { glm::vec2(0.5f, -0.5f) };
+	quad.d = { glm::vec2(-0.5f, -0.5f) };
+
+	PushDrawData(GL_LINES, reinterpret_cast<Vertex*>(&yAxis), xAxis.Vertices());
+	PushDrawData(GL_LINE_LOOP, reinterpret_cast<Vertex*>(&quad), quad.Vertices());
+	PushDrawData(GL_LINES, reinterpret_cast<Vertex*>(&xAxis), xAxis.Vertices());
+
+ 	m_Camera.fov = 45.0f;
+ 	m_Camera.position = glm::vec3(0.f, 0.f, 3.f);
+ 	m_Camera.lookPosition = glm::vec3(0.f);
+ 	m_Camera.up = glm::vec3(0.f, 1.f, 0.f);
+
+	::projection = glm::perspective(m_Camera.fov, m_Window->AspectRatio(), 0.1f, 1000.0f);
+	::view = glm::lookAt(m_Camera.position, m_Camera.lookPosition, m_Camera.up);
+	::model = glm::mat4(1.f);
 }
 
 // This method will be called each frame in main loop
 void Renderer::Render() 
 {
-	Shader::Bind(FindShader("Basic"));
-	Vertex pos[5] {
-		{glm::vec2(-0.5f, 0.5f)},
-		{glm::vec2(0.5f, 0.5f)},
-		{glm::vec2(0.5f, -0.5f)},
-		{glm::vec2(-0.5f, -0.5f)},
-		{glm::vec2(-0.5f, 0.5f)},
-	};
-	// std::cout << sizeof(pos) << std::endl;
-	glBufferSubData(GL_ARRAY_BUFFER, 0, 5 * sizeof(Vertex), &pos[0]);
-	glDrawArrays(GL_LINE_STRIP, 0, 5);
+	Shader::Bind(*basicShader);
+	
+	// glm::mat4
+	basicShader->SetUniformMat4("u_Projection", ::projection);
+	basicShader->SetUniformMat4("u_View", ::view);
+	basicShader->SetUniformMat4("u_Model", ::model);
+
+	FlushDrawData();
+
+	::projection = glm::perspective(m_Camera.fov, m_Window->AspectRatio(), 0.1f, 1000.0f);
+	::view = glm::lookAt(m_Camera.position, m_Camera.lookPosition, m_Camera.up);
 }
 
 // This ImGui context method will be called each frame in main loop
@@ -73,9 +107,11 @@ void Renderer::ImGuiRender()
 
 }
 
-void Renderer::MapShader(const char* name)
+std::shared_ptr<Shader> Renderer::MapShader(const char* name)
 {
-	m_ShaderMap.insert(std::make_pair( name, std::make_shared<Shader>(name) ));
+	std::shared_ptr<Shader> shader = std::make_shared<Shader>(name);
+	m_ShaderMap.insert(std::make_pair( name, shader ));
+	return shader;
 }
 
 Shader& Renderer::FindShader(const char* name)
@@ -87,4 +123,22 @@ Shader& Renderer::FindShader(const char* name)
 	}
 
 	return *iter->second;
+}
+
+void Renderer::PushDrawData(unsigned int drawMode, Vertex* data, size_t count)
+{
+	std::vector<Vertex>& vec = m_DynamicBatching[drawMode];
+	for (size_t i = 0; i < count; i++)
+	{
+		vec.push_back(*(data + i));
+	}
+}
+
+void Renderer::FlushDrawData()
+{
+	for (auto& pair : m_DynamicBatching)
+	{
+		glBufferSubData(GL_ARRAY_BUFFER, 0, pair.second.size() * sizeof(Vertex), pair.second.data());
+		glDrawArrays(pair.first, 0, pair.second.size());
+	}
 }
--- Source/Core/Renderer.h
+++ Source/Core/Renderer.h
@@ -1,36 +1,75 @@
 #pragma once
 
 #include <Core/Shader.h>
+#include <Core/Camera.h>
 
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <unordered_map>
+#include <vector>
 
 struct Vertex
 {
 	glm::vec2 position;
 };
 
+struct Line
+{
+	Vertex a, b;
+	inline constexpr size_t Vertices() const { return 2; }
+};
+
+struct Triangle
+{
+	Vertex a, b, c;
+	inline constexpr size_t Vertices() const { return 3; }
+};
+
+struct Quad
+{
+	Vertex a, b, c, d;
+	inline constexpr size_t Vertices() const { return 4; }
+};
+
+// forward declaration
+class Window;
+
 class Renderer
 {
 public:
 	using ShaderMap = std::unordered_map<const char*, std::shared_ptr<Shader>>;
+	using DynamicBatchMap = std::unordered_map<unsigned int, std::vector<Vertex>>;
 public:
-	Renderer(GLFWwindow* context, size_t vertices);
+	Renderer(Window* const window, size_t vertices);
 	~Renderer() = default;
 
 	void Render();
 	void ImGuiRender();
 
-	void MapShader(const char* name);
+	std::shared_ptr<Shader> MapShader(const char* name);
 	Shader& FindShader(const char* name);
-private:
-	GLFWwindow* m_Context;
-	
+
+	void PushDrawData(unsigned int drawMode, Vertex* data, size_t count);
+	void FlushDrawData();
+private:	
 	unsigned int m_VAO;
 	unsigned int m_VBO;
 	unsigned int m_Program;
 	size_t m_Vertices;
 
+	Window* const m_Window;
+
+	Camera m_Camera;
 	ShaderMap m_ShaderMap;
+	/*
+	Dynamic Batching map adds primitives into unordered map by a key - Draw Mode
+	As a reference I used Unity documentation on Dynamic Batching
+	https://docs.unity3d.com/Manual/dynamic-batching.html
+
+	It's good for low-poly or for low-line objects. 
+	Drawcall count will be equal to amount of buckets in unordered map, thus equal to Draw Mode amount
+	*/
+	DynamicBatchMap m_DynamicBatching;
 };
