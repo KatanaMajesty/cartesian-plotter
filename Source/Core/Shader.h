c63db26feaa629240880669af4e692cab696eda8
KatanaMajesty <sasha.panasenk@gmail.com>
2022-05-27 19:12:22 +0300

    Added MVP into Basic.vert & Added Uniform Support in Shader class

--- Source/Core/Shader.h
+++ Source/Core/Shader.h
@@ -1,14 +1,28 @@
 #pragma once
 
+#include <glm/glm.hpp>
+#include <unordered_map>
+
 class Shader
 {
 public:
 	Shader(const char* name);
 	~Shader();
 
+	inline constexpr unsigned int Program() const { return m_Program; }
+
+	void SetUniformMat4(const char* uniform, const glm::mat4& mat);
+	void SetUniformVec4(const char* uniform, const glm::vec4& vec);
+	void SetUniformVec3(const char* uniform, const glm::vec3& vec);
+	void SetUniformVec2(const char* uniform, const glm::vec2& vec);
+
 	static void Bind(const Shader& shader);
 private:
 	unsigned int m_Program;
 	unsigned int m_Vertex;
 	unsigned int m_Fragment;
+
+	// Uniform cache here is used in order to avoid redundant glGetUniformLocation calls
+	// unordered map will store uniform locations instead
+	std::unordered_map<const char*, unsigned int> m_UniformCache;
 };
